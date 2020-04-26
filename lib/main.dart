215e010401c3de9ee0e8b8bf4ea3bfd49eb1cdb8
Natalia <n.grzywalska@gmail.com>
2020-04-26 18:30:43 +0200

    2nd commit with 2 screen app

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,196 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:numberpicker/numberpicker.dart';
 
-//the starting point for the app
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Sleep Tracker'),
-            backgroundColor: Colors.amber[600],
-          ),
-          body: Column(
-            children: <Widget>[
-              Center(
-                child: Image(
-                  image: AssetImage('images/chmurka.png'),
-                ),
-              ),
-              // TODO: pokombinować z rich tekstem żeby uzyskać inny odcień czcionki
-              Text('Get to know your baby\'s sleep patterns and keep\n'
-                  ' track of how much sleep they are getting here.'),
-            ],
-          ),
-        ),
-      ),
-    );
+void main() {
+  runApp(SleepTracker());
+}
 
 class SleepTracker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: FirstScreen(),
+    );
   }
 }
 
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sleep Tracker'),
+        backgroundColor: Colors.amber[600],
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            height: 70.0,
+            width: 70.0,
+            // TODO find some better icon
+            child: Icon(Icons.brightness_3),
+          ),
+          Text(
+            'Get to know your baby\'s sleep patterns and keep\n'
+            ' track of how much sleep they are getting here.',
+            style: TextStyle(color: Colors.grey.shade700),
+          ),
+          SizedBox(
+            height: 25.0,
+            width: double.infinity,
+          ),
+          RaisedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                new MaterialPageRoute(builder: (context) => new SecondScreen()),
+              );
+            },
+            textColor: Colors.white,
+            padding: const EdgeInsets.all(0.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    Color(0xFF0D47A1),
+                    Color(0xFF1976D2),
+                    Color(0xFF42A5F5),
+                  ],
+                ),
+              ),
+              padding: const EdgeInsets.all(10.0),
+              child: const Text('Add new sleeping record',
+                  style: TextStyle(fontSize: 15)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String timeString;
+    return Scaffold(
+      appBar: AppBar(
+        //should add back button
+        automaticallyImplyLeading: true,
+        title: Text('Sleep Tracker'),
+        backgroundColor: Colors.amber[600],
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            height: 130,
+            child: Image(
+              image: AssetImage('images/cloud.png'),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.calendar_today,
+                color: Colors.indigo[900],
+              ),
+              title: Text(
+                'Date and time',
+                style: TextStyle(
+                  color: Colors.indigo[900],
+                  fontSize: 10.0,
+                ),
+              ),
+              subtitle: Text(
+                // TODO to modify
+                '${DateTime.now().day} ${DateTime.now().month} ${DateTime.now().year}, ${DateTime.now().hour} : ${DateTime.now().minute}',
+              ),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.brightness_3,
+                color: Colors.indigo[900],
+              ),
+              title: Text(
+                'Sleep type',
+                style: TextStyle(
+                  color: Colors.indigo[900],
+                  fontSize: 10.0,
+                ),
+              ),
+              subtitle: DropdownButton(
+                  // TODO
+                  ),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.access_time,
+                color: Colors.indigo[900],
+              ),
+              title: Text(
+                'Sleep duration',
+                style: TextStyle(
+                  color: Colors.indigo[900],
+                  fontSize: 10.0,
+                ),
+              ),
+              onTap: () {
+                NumberPicker.integer(
+                  initialValue: 50,
+                  minValue: 0,
+                  maxValue: 100,
+                  onChanged: null, //TODO
+                );
+              },
+            ),
+          ),
+          SizedBox(
+            height: 60,
+          ),
+          RaisedButton(
+            onPressed: () {
+//              Navigator.push(
+//                context,
+//                new MaterialPageRoute(builder: (context) => new SecondScreen()),
+//              );
+            },
+            textColor: Colors.white,
+            padding: const EdgeInsets.all(0.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    Color(0xFF0D47A1),
+                    Color(0xFF1976D2),
+                    Color(0xFF42A5F5),
+                  ],
+                ),
+              ),
+              padding: const EdgeInsets.all(10.0),
+              child: const Text('     Save     ', style: TextStyle(fontSize: 15)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 //
 //class MyApp extends StatelessWidget {
